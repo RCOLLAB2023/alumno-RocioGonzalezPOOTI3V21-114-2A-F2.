b8a22174ee82a79ab7d6cbb435570e39863b8fa7
Rocio Gonzalez <rocio.gonzalez40@inacapmail.cl>
2025-11-03 21:12:09 -0300

    actualizacion

--- clase03112025/clase/CuentaBancaria.py
+++ clase03112025/clase/CuentaBancaria.py
@@ -1,5 +1,24 @@
 class CuentaBancaria:
-    def __init__(self,numeroCuenta,titular,saldo)
+    def __init__(self,numeroCuenta,titular,saldo_inicial=0):
         self.numeroCuenta=numeroCuenta
         self.titular=titular
-        
+        self.saldo=saldo_inicial
+
+    def depositar(self,monto):
+        if monto > 0:
+            self.saldo += monto
+            print(f"Deposito exitoso de ${monto}")
+        else:
+            print("el monto a depositar debe ser positivo")
+
+    def retirar (self,monto):
+        if monto <= 0:
+            print("elmonto a retirar debe ser positivo")
+        elif monto > self.saldo:
+            print("Fondos insuficientes")    
+        else:
+            self.saldo-= monto
+            print(f"Retiro exitoso de ${monto}")    
+
+
+            
